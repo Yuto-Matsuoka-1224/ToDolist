0e98420fb88a394bf73c22271f5b0d0ba073cbc5
Yuto Matsuoka <52948621+Yuto-Matsuoka-1224@users.noreply.github.com>
2021-05-23 15:38:16 +0900

    #8 RATE色分け (#9)
    * [add] #1 a small change
    
    * [add] #3 RATE addition
    
    * [add] #8 RATE color changed

--- resources/views/back/tasks/complete.blade.php
+++ resources/views/back/tasks/complete.blade.php
@@ -52,7 +52,16 @@ $title = 'タスク完了一覧';
                $predict_time = $task->predicttime_hours * 60 + $task->predicttime_minutes;
                $RATES = $real_time/$predict_time;
                $RATE = number_format($RATES, 2); ?>
-             <td style="color:#ff0000; text-align: center;">{{ $RATE }}</td>
+             
+             <!-- RATEの色分け -->
+             @if ($RATE >= 1.80)
+                <td style="color:red; text-align: center;">{{ $RATE }}</td>
+             @elseif ($RATE >= 0.70)
+                <td style="color:black; text-align: center;">{{ $RATE }}</td>
+             @else
+                <td style="color:blue; text-align: center;">{{ $RATE }}</td>
+             @endif
+             
         </tr>
         @endforeach
         </tbody>
